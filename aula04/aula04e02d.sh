8c1d8ad6c9eb4bb399610e5c3418149ec3c918e4
tiagofcvale <tiagofcvale@ua.pt>
2025-10-09 10:27:41 +0100

    correcao

--- aula04/aula04e02d.sh
+++ aula04/aula04e02d.sh
@@ -2,21 +2,28 @@
 
 function comparator() {
     if [[ "$1" -eq "$2" ]]; then
-        return 1
-    else
         return 0
+    else
+        if [[ "$1" -gt "$2" ]]; then
+            return 1
+        else
+            return 2
+        fi
     fi
 }
 
 echo "Digite dois números"
-read num1
-read num2
+read num1 num2
 comparator $num1 $num2
 
 valor_retorno=$?
 
-if [[ $valor_retorno -eq 1 ]]; then
+if [[ $valor_retorno -eq 0 ]]; then
     echo "Números iguais"
 else
-    echo "Números diferentes"
+    if [[ $valor_retorno -eq 1 ]]; then
+        echo "$num1 é maior"
+    else 
+        echo "$num2 é maior"
+    fi
 fi
