4fbff5ee2182e54f116683218906db24b051d57b
tiagofcvale <tiagofcvale@ua.pt>
2025-10-23 10:13:54 +0100

    strtod

--- aula05/args1.c
+++ aula05/args1.c
@@ -4,6 +4,7 @@
 int main(int argc, char *argv[]) {
     if (argc != 3) { // 3 -> 2 argumentos + 1 ./args1
         printf("ERRO! Devem ser introduzidos exatamente 2 argumentos\n");
+        return EXIT_FAILURE;
     }
     for (int i = 0; i < argc; i++)
         printf("Argument %02d: \"%s\"\n",i,argv[i]);
