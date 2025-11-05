2514728e53bd54f65b639fcae7dcacadd3da6ddb
tiagofcvale <tiagofcvale@ua.pt>
2025-11-05 02:19:12 +0000

    3

--- aula06/myActions.c
+++ aula06/myActions.c
@@ -1,46 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-/* SUGESTÂO: utilize as páginas do manual para conhecer mais sobre as funções usadas:
- man system
- man date
-*/
-
-int main(int argc, char *argv[])
+int main(void)
 {
     char text[1024];
     FILE *fp = NULL;
 
-    fp = fopen("command.log", "w");
-
+    fp = fopen("command.log", "a");
     if (fp == NULL) {
-        perror("Erro ao abrir ou criar o arquivo");
+        perror("Erro ao abrir ou criar o ficheiro");
         return EXIT_FAILURE;
     }
 
-    do
-    {
+    while (1) {
         printf("Command: ");
-        scanf("%1023[^\n]%*c", text);
-
-        /* system(const char *command) executes a command specified in command
-            by calling /bin/sh -c command, and returns after the command has been
-            completed.
-        */
-        if(strcmp(text, "end")) {
-           printf("\n * Command to be executed: %s\n", text);
-           printf("---------------------------------\n");
-           system(text);
-           printf("---------------------------------\n");
-           fprintf(fp, "Command to be executed: %s\n", text);
-           fprintf(fp, "---------------------------------");
-           fprintf(fp, system(text));
-           fprintf(fp, "---------------------------------");
+        if (fgets(text, sizeof text, stdin) == NULL) { 
+            putchar('\n');
+            break;
         }
-    } while(strcmp(text, "end"));
+
+        size_t len = strcspn(text, "\n");
+        text[len] = '\0';
+
+        if (strcmp(text, "end") == 0) {
+            break;
+        }
+
+        time_t now = time(NULL);
+        struct tm *lt = localtime(&now);
+        char timestr[64];
+        if (lt != NULL) {
+            strftime(timestr, sizeof timestr, "%Y-%m-%d %H:%M:%S", lt);
+        } else {
+            strncpy(timestr, "unknown-time", sizeof timestr);
+            timestr[sizeof timestr - 1] = '\0';
+        }
+
+        
+        printf("\n * Command to be executed: %s\n", text);
+        printf("---------------------------------\n");
+        system(text);
+        printf("---------------------------------\n");
+
+        fprintf(fp, "[%s] %s\n", timestr, text);
+        fflush(fp); 
+    }
+
     fclose(fp);
     printf("-----------The End---------------\n");
-
     return EXIT_SUCCESS;
 }
