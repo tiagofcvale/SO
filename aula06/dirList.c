21b3f52781131d7064ed17a1b8e95c5f808c6db8
tiagofcvale <tiagofcvale@ua.pt>
2025-11-05 02:30:04 +0000

    4

--- aula06/dirList.c
+++ aula06/dirList.c
@@ -1,41 +1,57 @@
 #include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-/* SUGESTÂO: utilize as páginas do manual para conhecer mais sobre as funções usadas:
-  man opendir
-  man readdir
-*/
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 void listDir(char dirname[])
 {
     DIR *dp; 
     struct dirent *dent;
+    char path[1024];
+    struct stat st;
 
     dp = opendir(dirname); 
-    if(dp == NULL)
+    if (dp == NULL)
     {
         perror("Error opening directory");
         return;
     }
 
-    dent = readdir(dp);
-    while(dent!=NULL) 
+    while ((dent = readdir(dp)) != NULL) 
     {
-        if(dent->d_name[0] != '.') /* do not list hidden dirs/files */
+        if (dent->d_name[0] == '.')
+            continue;
+
+        snprintf(path, sizeof(path), "%s/%s", dirname, dent->d_name);
+
+        if (stat(path, &st) == -1)
         {
-          printf("%s/%s\n",dirname,dent->d_name);
+            perror("Error reading file info");
+            continue;
         }
 
-        dent = readdir(dp);
+        if (S_ISDIR(st.st_mode))
+        {
+            printf("d %s\n", path);
+
+            listDir(path); // recursive
+        }
+        else
+        {
+            printf("  %s\n", path);
+        }
     }
+
+    closedir(dp);
 }
 
 int main(int argc, char *argv[])
 {
-    if(argc != 2)
+    if (argc != 2)
     {
-        fprintf(stderr,"Usage: %s base_directory\n",argv[0]);
+        fprintf(stderr, "Usage: %s base_directory\n", argv[0]);
         return EXIT_FAILURE;
     }
 
@@ -43,4 +59,3 @@ int main(int argc, char *argv[])
     
     return EXIT_SUCCESS;
 }
-
