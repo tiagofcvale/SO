b598d5fd2981e6f737b45eb280b2683784c381ad
tiagofcvale <tiagofcvale@ua.pt>
2025-12-11 09:59:04 +0000

    c

--- aula12/pipe1.c
+++ aula12/pipe1.c
@@ -3,25 +3,37 @@
 
 int main(int argc, char *argv[])
 {
-    int fdp[2];
+    int fdp1[2], fdp2[2];
     char buf[1024];
 
-    if(pipe(fdp)!=0) {
+    if(pipe(fdp1)!=0) {
         perror(argv[0]);
         return 1;
+    }
+
+    if(pipe(fdp2)!=0){
+       perror(argv[0]);
+       return 1;
     }
 
     switch(fork()) {
        case -1:
               perror(argv[0]);
               return 1;
-       case 0:
-              close(fdp[0]);
-              write(fdp[1],"Hello Dad!",11);
+       case 0: // FILHO
+              close(fdp1[0]);
+              close(fdp2[1]);  // O filho só vai ler no pipe2 e só vai escrever no pipe1
+              write(fdp1[1],"Hello Dad!",11); //escreve no pipe1
+              close(fdp1[1]); // fecha o pipe1
+              read(fdp2[0],buf,1024); // lê no pipe2
+              printf("Child: %s\n",buf); 
               break;
-       default:
-              close(fdp[1]);
-              read(fdp[0],buf,1024);
+       default: // PAI
+              close(fdp1[1]);
+              close(fdp2[0]); // O pai só vai escrever no pipe2 e só vai ler no pipe1
+              read(fdp1[0],buf,1024); // Lê no pipe1
+              close(fdp1[0]); // fecha o pipe1
+              write(fdp2[1],"Good Morning!",14); // Escreve no pipe2
               printf("Parent: %s\n",buf);
    }
    return 0;
